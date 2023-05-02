e37551d0e7f09174cc71aeb6d063a912375ce9c9
abhinandpn <abhinandpn40672gmail.com>
2023-05-02 20:40:47 +0530

    connection file and automigrating is finaling

--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -15,19 +15,46 @@ func ConnectDatabase(cfg config.Config) (*gorm.DB, error) {
 	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
 		SkipDefaultTransaction: true,
 	})
-	// User Table
-	db.AutoMigrate(&domain.Users{})
-	db.AutoMigrate(&domain.UserInfo{})
-	db.AutoMigrate(&domain.Address{})
+	err := db.AutoMigrate(
 
-	// Admin Table
-	db.AutoMigrate(&domain.Admins{})
+		// User Table
+		&domain.Users{},
+		&domain.UserInfo{},
+		&domain.Address{},
 
-	// Product Table
-	// cart Table
-	// Wish List
-	// order Table
-	// Payment Detail
+		// Admin Table
+		&domain.Admins{},
+
+		// Product Table
+		&domain.Categery{},
+		&domain.ProductBrand{},
+		&domain.Product{},
+		&domain.ProductItem{},
+		&domain.Coupons{},
+		// cart Table
+		&domain.Cart{},
+		&domain.CartItems{},
+
+		// Wish List
+		&domain.Wishlist{},
+		&domain.WishlistItem{},
+
+		// order Table
+		&domain.Order{},
+		&domain.OrderLine{},
+		&domain.OrderStatus{},
+		&domain.DeliveryStatus{},
+		&domain.Return{},
+
+		// Payment Detail
+		&domain.PaymentStatus{},
+		&domain.PaymentDetails{},
+	)
+
+	// Error handling For Database Table creating
+	if err != nil {
+		return nil, err
+	}
 
 	return db, dbErr
 }
