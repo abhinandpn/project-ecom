031f932876aa291b0d7a7186c878363c5605d9d7
abhinandpn <abhinandpn40672gmail.com>
2023-05-04 17:56:29 +0530

    main file is finaling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,12 +8,15 @@ import (
 )
 
 func main() {
+
 	cfg, configErr := config.LoadConfig()
+
 	if configErr != nil {
 		log.Fatal("cannot load config: ", configErr)
 	}
 
 	server, diErr := di.InitializeAPI(cfg)
+
 	if diErr != nil {
 		log.Fatal("cannot start server: ", diErr)
 	} else {
