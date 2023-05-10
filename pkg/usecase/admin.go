487bbb04eea64d9a0ca39e4adaf3c73a3e15121e
abhinandpn <abhinandpn40672gmail.com>
2023-05-10 21:24:06 +0530

    admin

--- pkg/usecase/admin.go
+++ pkg/usecase/admin.go
@@ -1,8 +1,13 @@
 package usecase
 
 import (
+	"context"
+	"errors"
+
+	domain "github.com/abhinandpn/project-ecom/pkg/domain"
 	interfaces "github.com/abhinandpn/project-ecom/pkg/repository/interface"
 	services "github.com/abhinandpn/project-ecom/pkg/usecase/interfaces"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AdminUseCase struct {
@@ -12,4 +17,20 @@ type AdminUseCase struct {
 func NewAdminUseCase(repo interfaces.AdminRepository) services.AdminUseCase {
 
 	return &AdminUseCase{adminRepo: repo}
+}
+func (adm *AdminUseCase) Login(ctx context.Context, admin domain.Admin) (domain.Admin, error) {
+	// Get the admin from DB
+	dbAdmin, err := adm.adminRepo.FindAdmin(ctx, admin)
+	if err != nil {
+		return admin, err
+	} else if dbAdmin.ID == 0 {
+		return admin, errors.New("admin not exist")
+
+	}
+	// Check the pass and username
+	if bcrypt.CompareHashAndPassword([]byte(dbAdmin.Password), []byte(admin.Password)) != nil {
+		return admin, errors.New("wrong password")
+	}
+
+	return dbAdmin, nil
 }
