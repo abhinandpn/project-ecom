ce6c4e076b252d30bcda98539380e37496d5d001
abhinandpn <abhinandpn40672gmail.com>
2023-05-10 18:50:17 +0530

    admin

--- pkg/usecase/admin.go
+++ pkg/usecase/admin.go
@@ -1,1 +1,15 @@
 package usecase
+
+import (
+	interfaces "github.com/abhinandpn/project-ecom/pkg/repository/interface"
+	services "github.com/abhinandpn/project-ecom/pkg/usecase/interfaces"
+)
+
+type AdminUseCase struct {
+	adminRepo interfaces.AdminRepository
+}
+
+func NewAdminUseCase(repo interfaces.AdminRepository) services.AdminUseCase {
+
+	return &AdminUseCase{adminRepo: repo}
+}
