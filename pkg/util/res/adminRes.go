7fda00aa323d2367925decbdf7665a864756ae37
abhinandpn <abhinandpn40672gmail.com>
2023-05-10 18:51:09 +0530

    admin responce repo

--- pkg/util/res/adminRes.go
+++ pkg/util/res/adminRes.go
@@ -3,7 +3,7 @@ package res
 var ResponseMap map[string]string
 
 type AdminLoginRes struct {
-	ID       uint   `json:"id" `
+	ID       uint   `json:"id"`
 	UserName string `json:"username"`
 	Email    string `json:"email"`
 }
