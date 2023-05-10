3cc3c63fd3fec0456da4039fc91d6f6b330cb433
abhinandpn <abhinandpn40672gmail.com>
2023-05-10 18:53:21 +0530

    updating

--- pkg/domain/favourites.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Favourite struct {
-	Id          uint `gorm:"primaryKey;unique;not null"`
-	UserId      uint
-	Users       Users `gorm:"foreignKey:UserId"`
-	ItemId      uint
-	ProductItem ProductItem `gorm:"foreignKey:ItemId"`
-}
