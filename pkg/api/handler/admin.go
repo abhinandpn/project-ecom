916b7843957d5b34f84dfde390c41817910583ad
abhinandpn <abhinandpn40672gmail.com>
2023-05-10 21:23:05 +0530

    admin login function added and final

--- pkg/api/handler/admin.go
+++ pkg/api/handler/admin.go
@@ -1,1 +1,75 @@
 package handler
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/abhinandpn/project-ecom/pkg/auth"
+	"github.com/abhinandpn/project-ecom/pkg/domain"
+	services "github.com/abhinandpn/project-ecom/pkg/usecase/interfaces"
+	"github.com/abhinandpn/project-ecom/pkg/util/req"
+	"github.com/abhinandpn/project-ecom/pkg/util/res"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/copier"
+)
+
+type AdminHandler struct {
+	AdminUseCase services.AdminUseCase
+}
+
+func NewAdminHandler(AdminUseCase services.AdminUseCase) *AdminHandler {
+	return &AdminHandler{AdminUseCase: AdminUseCase}
+}
+
+// AdminLogin godoc
+// @summary api for admin to login
+// @id AdminLogin
+// @tags Admin Login
+// @Param input body req.LoginStruct{} true "inputs"
+// @Router /admin/login [post]
+// @Success 200 {object} res.Response{} "successfully logged in"
+// @Failure 400 {object} res.Response{} "invalid input"
+// @Failure 500 {object} res.Response{} "faild to generate jwt token"
+
+func (adm *AdminHandler) AdminLogin(ctx *gin.Context) {
+
+	var body req.AdminLoginStruct
+
+	// Admin Binding and error handling
+	err := ctx.ShouldBindJSON(&body)
+	if err != nil {
+		response := res.ErrorResponse(400, "invalid input", err.Error(), body)
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// check the fields
+	if body.Email == "" && body.UserName == "" {
+		err := errors.New("enter email or user_name atleast")
+		response := res.ErrorResponse(400, "invalid input", err.Error(), body)
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	var admin domain.Admin
+	copier.Copy(&admin, &body)
+	admin, err = adm.AdminUseCase.Login(ctx, admin)
+
+	if err != nil {
+		response := res.ErrorResponse(400, "faild to login", err.Error(), admin)
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	tokenString, err := auth.GenerateJWT(admin.ID)
+	if err != nil {
+		response := res.ErrorResponse(500, "faild to generate jwt token", err.Error(), nil)
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	ctx.SetCookie("admin-auth", tokenString["accessToken"], 60*60, "", "", false, true)
+
+	response := res.SuccessResponse(200, "successfully logged in", nil)
+	ctx.JSON(http.StatusOK, response)
+}
