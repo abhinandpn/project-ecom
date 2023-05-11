346a47013607af33177e07514dc6898ab8f98ab6
abhinandpn <abhinandpn40672gmail.com>
2023-05-11 20:01:10 +0530

    user interface

--- pkg/usecase/interfaces/userinterface.go
+++ pkg/usecase/interfaces/userinterface.go
@@ -7,6 +7,11 @@ import (
 )
 
 type UserUseCase interface {
+
+	// Signup
+	// SignUp(ctx context.Context, user domain.Users) error
+
+	//........................................................
 	FindAll(ctx context.Context) ([]domain.Users, error)
 	FindByID(ctx context.Context, id uint) (domain.Users, error)
 	Save(ctx context.Context, user domain.Users) (domain.Users, error)
