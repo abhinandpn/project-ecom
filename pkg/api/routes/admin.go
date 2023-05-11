8b247da1373bb851aa76fa29f34e832e2a726113
abhinandpn <abhinandpn40672gmail.com>
2023-05-11 11:16:12 +0530

    admin route

--- pkg/api/routes/admin.go
+++ pkg/api/routes/admin.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	"github.com/abhinandpn/project-ecom/pkg/api/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRoute(api *gin.RouterGroup,
+	AdminHandler *handler.AdminHandler,
+) {
+	// Login
+	login := api.Group("/admin")
+	{
+		login.POST("/", AdminHandler.AdminLogin)
+	}
+}
