a40b005d5595169b17f69b8c6910487ef2db37f3
abhinandpn <abhinandpn40672gmail.com>
2023-05-11 19:59:48 +0530

    user signup function and depents

--- pkg/repository/interface/userinterface.go
+++ pkg/repository/interface/userinterface.go
@@ -7,6 +7,14 @@ import (
 )
 
 type UserRepository interface {
+
+	// ..............................................
+	FindUser(ctx context.Context, user domain.Users) (domain.Users, error)
+	FindUserByEmail(ctx context.Context, email string) (user domain.Users, err error)
+	FindUserByNumber(ctx context.Context, number uint) (user domain.Users, err error)
+	SaveUser(ctx context.Context, user domain.Users) (UserId uint, err error)
+	// ..............................................
+
 	FindAll(ctx context.Context) ([]domain.Users, error)
 	FindByID(ctx context.Context, id uint) (domain.Users, error)
 	Save(ctx context.Context, user domain.Users) (domain.Users, error)
