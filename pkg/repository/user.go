aa485f80d069cd1377a49e6a04b0a7127bd6fb1e
abhinandpn <abhinandpn40672gmail.com>
2023-05-11 20:00:34 +0530

    user functions signup

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 
 	domain "github.com/abhinandpn/project-ecom/pkg/domain"
 	interfaces "github.com/abhinandpn/project-ecom/pkg/repository/interface"
@@ -13,9 +16,74 @@ type userDatabase struct {
 }
 
 func NewUserRepository(DB *gorm.DB) interfaces.UserRepository {
-	return &userDatabase{DB}
+	return &userDatabase{DB: DB}
 }
 
+// .....................................................
+// Find user by details
+func (usr *userDatabase) FindUser(ctx context.Context, user domain.Users) (domain.Users, error) {
+	query := `select * from user where id = ? or email = ? or phone = ? or username = ? `
+
+	err := usr.DB.Raw(query, user.ID, user.Email, user.Number, user.UserName)
+	if err != nil {
+		return user, errors.New("faild to get user")
+	}
+	return user, nil
+}
+
+// Find user By mail id
+func (usr *userDatabase) FindUserByEmail(ctx context.Context, email string) (user domain.Users, err error) {
+
+	query := `SELECT * FROM users WHERE email = $1`
+
+	err = usr.DB.Raw(query, email).Scan(&user).Error
+
+	if err != nil {
+
+		return user, fmt.Errorf("faild to find user with email %v", email)
+
+	}
+
+	fmt.Println("\n\ndb user detail", user)
+
+	return user, nil
+}
+
+// find user by num
+func (usr *userDatabase) FindUserByNumber(ctx context.Context, number uint) (user domain.Users, err error) {
+	query := `select * from users where phone = $1`
+
+	err = usr.DB.Raw(query, number).Scan(&user).Error
+
+	if err != nil {
+		return user, fmt.Errorf("faild to find user with number %v", number)
+
+	}
+
+	fmt.Println("\n\ndb user detail", user)
+
+	return user, nil
+}
+
+// save user
+func (usr *userDatabase) SaveUser(ctx context.Context, user domain.Users) (UserId uint, err error) {
+
+	query := `insert in to users (username,f_name,l_name,email,phone,password,created_at)
+	Values ($1 ,$2 ,$3 ,$4 ,$5 ,$6 ,$7)`
+
+	createdAt := time.Now()
+	err = usr.DB.Raw(query, user.UserName, user.FName, user.LName,
+		user.Email, user.Number, user.Password, createdAt).Scan(&user).Error
+
+	if err != nil {
+
+		return 0, fmt.Errorf("faild to save user %s", user.UserName)
+	}
+
+	return UserId, nil
+}
+
+// .....................................................
 func (c *userDatabase) FindAll(ctx context.Context) ([]domain.Users, error) {
 	var users []domain.Users
 	err := c.DB.Find(&users).Error
