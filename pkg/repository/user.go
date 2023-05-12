04900aab231408a23cb0ecbf60a022b77504841a
abhinandpn <abhinandpn40672gmail.com>
2023-05-12 18:54:10 +0530

    login

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -84,28 +84,3 @@ func (usr *userDatabase) SaveUser(ctx context.Context, user domain.Users) (UserI
 }
 
 // .....................................................
-func (c *userDatabase) FindAll(ctx context.Context) ([]domain.Users, error) {
-	var users []domain.Users
-	err := c.DB.Find(&users).Error
-
-	return users, err
-}
-
-func (c *userDatabase) FindByID(ctx context.Context, id uint) (domain.Users, error) {
-	var user domain.Users
-	err := c.DB.First(&user, id).Error
-
-	return user, err
-}
-
-func (c *userDatabase) Save(ctx context.Context, user domain.Users) (domain.Users, error) {
-	err := c.DB.Save(&user).Error
-
-	return user, err
-}
-
-func (c *userDatabase) Delete(ctx context.Context, user domain.Users) error {
-	err := c.DB.Delete(&user).Error
-
-	return err
-}
