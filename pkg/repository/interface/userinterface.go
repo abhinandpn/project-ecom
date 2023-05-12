31378ec891f3b4415ec4f4100ef92da77e604712
abhinandpn <abhinandpn40672gmail.com>
2023-05-12 18:54:25 +0530

    login

--- pkg/repository/interface/userinterface.go
+++ pkg/repository/interface/userinterface.go
@@ -15,8 +15,4 @@ type UserRepository interface {
 	SaveUser(ctx context.Context, user domain.Users) (UserId uint, err error)
 	// ..............................................
 
-	FindAll(ctx context.Context) ([]domain.Users, error)
-	FindByID(ctx context.Context, id uint) (domain.Users, error)
-	Save(ctx context.Context, user domain.Users) (domain.Users, error)
-	Delete(ctx context.Context, user domain.Users) error
 }
