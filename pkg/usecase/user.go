ce902948955da9bc0a179368e4d4d4d1a9f0f22e
abhinandpn <abhinandpn40672gmail.com>
2023-05-12 18:40:47 +0530

    user

--- pkg/usecase/user.go
+++ pkg/usecase/user.go
@@ -7,7 +7,7 @@ import (
 	domain "github.com/abhinandpn/project-ecom/pkg/domain"
 	"github.com/abhinandpn/project-ecom/pkg/helper"
 	interfaces "github.com/abhinandpn/project-ecom/pkg/repository/interface"
-	services "github.com/abhinandpn/project-ecom/pkg/usecase/interfaces"
+	service "github.com/abhinandpn/project-ecom/pkg/usecase/interfaces"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,10 +15,8 @@ type userUseCase struct {
 	userRepo interfaces.UserRepository
 }
 
-func NewUserUseCase(repo interfaces.UserRepository) services.UserUseCase {
-	return &userUseCase{
-		userRepo: repo,
-	}
+func NewUserUseCase(repo interfaces.UserRepository) service.UserUseCase {
+	return &userUseCase{userRepo: repo}
 }
 
 // ........................................
@@ -60,24 +58,3 @@ func (usr *userUseCase) SignUp(ctx context.Context, user domain.Users) error {
 }
 
 // ........................................
-func (c *userUseCase) FindAll(ctx context.Context) ([]domain.Users, error) {
-	users, err := c.userRepo.FindAll(ctx)
-	return users, err
-}
-
-func (c *userUseCase) FindByID(ctx context.Context, id uint) (domain.Users, error) {
-	user, err := c.userRepo.FindByID(ctx, id)
-	return user, err
-}
-
-func (c *userUseCase) Save(ctx context.Context, user domain.Users) (domain.Users, error) {
-	user, err := c.userRepo.Save(ctx, user)
-
-	return user, err
-}
-
-func (c *userUseCase) Delete(ctx context.Context, user domain.Users) error {
-	err := c.userRepo.Delete(ctx, user)
-
-	return err
-}
