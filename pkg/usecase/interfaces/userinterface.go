0bd30cbeba20e7a06cb967a0e733c497231a2bc2
abhinandpn <abhinandpn40672gmail.com>
2023-05-12 18:40:28 +0530

    user

--- pkg/usecase/interfaces/userinterface.go
+++ pkg/usecase/interfaces/userinterface.go
@@ -12,8 +12,4 @@ type UserUseCase interface {
 	SignUp(ctx context.Context, user domain.Users) error
 
 	//........................................................
-	FindAll(ctx context.Context) ([]domain.Users, error)
-	FindByID(ctx context.Context, id uint) (domain.Users, error)
-	Save(ctx context.Context, user domain.Users) (domain.Users, error)
-	Delete(ctx context.Context, user domain.Users) error
 }
