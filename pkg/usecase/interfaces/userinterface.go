fdb5b805731953917875b11138632692ab8e4044
abhinandpn <abhinandpn40672gmail.com>
2023-05-12 18:54:47 +0530

    login

--- pkg/usecase/interfaces/userinterface.go
+++ pkg/usecase/interfaces/userinterface.go
@@ -10,6 +10,6 @@ type UserUseCase interface {
 
 	// Signup
 	SignUp(ctx context.Context, user domain.Users) error
-
+	Login(ctx context.Context, user domain.Users) (domain.Users, error)
 	//........................................................
 }
