96d2175ddc9327b2813770352e98ecf155cf68b4
abhinandpn <abhinandpn40672gmail.com>
2023-05-13 18:03:38 +0530

    user signip

--- pkg/api/routes/user.go
+++ pkg/api/routes/user.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"github.com/abhinandpn/project-ecom/pkg/api/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(api *gin.RouterGroup, userHandler *handler.UserHandler) {
+	// login
+	login := api.Group("/login")
+	{
+		login.POST("/", userHandler.UserLogin)
+	}
+	// Signup
+	signup := api.Group("/signup")
+	{
+		signup.POST("/", userHandler.UserSignUp)
+	}
+}
