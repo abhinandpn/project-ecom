0ddd390fc7d50f3351261d7e8fa9b1b269f5f328
abhinandpn <abhinandpn40672gmail.com>
2023-05-14 19:53:58 +0530

    admin - user

--- pkg/usecase/interfaces/admininterface.go
+++ pkg/usecase/interfaces/admininterface.go
@@ -4,8 +4,14 @@ import (
 	"context"
 
 	domain "github.com/abhinandpn/project-ecom/pkg/domain"
+	"github.com/abhinandpn/project-ecom/pkg/util/req"
+	"github.com/abhinandpn/project-ecom/pkg/util/res"
 )
 
 type AdminUseCase interface {
+	// Admin side
 	Login(ctx context.Context, admin domain.Admin) (domain.Admin, error)
+	// User side
+	FindAllUser(ctx context.Context, pagination req.PageNation) (users []res.UserResStruct, err error)
+	BlockUser(ctx context.Context, UserId uint) error
 }
