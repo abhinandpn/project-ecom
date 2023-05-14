e9d6d7d78581998f48618b7d78d130074348603b
abhinandpn <abhinandpn40672gmail.com>
2023-05-14 19:50:42 +0530

    admin-user

--- pkg/repository/interface/admininterface.go
+++ pkg/repository/interface/admininterface.go
@@ -5,9 +5,14 @@ import (
 
 	"github.com/abhinandpn/project-ecom/pkg/domain"
 	"github.com/abhinandpn/project-ecom/pkg/util/req"
+	"github.com/abhinandpn/project-ecom/pkg/util/res"
 )
 
 type AdminRepository interface {
+	// Admin Main
 	CreateAdmin(req.AdminLoginStruct) error
 	FindAdmin(ctx context.Context, admin domain.Admin) (domain.Admin, error)
+	// user Side
+	ListAllUser(ctx context.Context, PageNation req.PageNation) (user []res.UserResStruct, err error)
+	BlockUser(ctx context.Context, userId uint) error
 }
