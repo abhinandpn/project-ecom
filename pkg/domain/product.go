89052615d8b292a081ff9910a258510b52948bfa
abhinandpn <abhinandpn40672gmail.com>
2023-05-14 19:49:48 +0530

    product managment

--- pkg/domain/product.go
+++ pkg/domain/product.go
@@ -3,20 +3,49 @@ package domain
 import "time"
 
 type Product struct {
-	Id            uint
-	ProductName   string
-	Discription   string
-	CategoryId    uint
-	Category      Category
-	Price         uint
-	DiscountPrice uint
-	Image         string
-	CreatedAt     time.Time
-	UpdateAt      time.Time
+	Id            uint        `json:"id" gorm:"primaryKey;not null"`
+	ProductName   string      `json:"product_name" gorm:"not null" binding:"required,min=3,max=50"`
+	Discription   string      `json:"description" gorm:"not null" binding:"required,min=10,max=100"`
+	CategoryId    uint        `json:"category_id" binding:"omitempty,numeric"`
+	Category      Category    `json:"-"`
+	Brand         Brand       `json:"-"`
+	Price         uint        `json:"price" gorm:"not null" binding:"required,numeric"`
+	DiscountPrice uint        `json:"discount_price"`
+	Info          ProductInfo `json:"-"`
+	Image         string      `json:"image" gorm:"not null"`
+	CreatedAt     time.Time   `json:"created_at" gorm:"not null"`
+	UpdateAt      time.Time   `json:"updated_at"`
+}
+type ProductInfo struct {
+	Id        uint   `json:"id" gorm:"primaryKey;not null"`
+	ProductId uint   `json:"product_id" binding:"omitempty,numeric"`
+	Colour    string `json:"colour"`
+	Size      uint   `json:"size" binding:"required,numeric"`
+	Brand     uint   `json:"brand"`
 }
 type Category struct {
-	Id           uint
-	CategoryID   uint `json:"catrgory_id"`
-	Category     *Category
-	CategoryName string `json:"category_name" gorm:"unique;not null" binding:"required,min=3,max=30"`
+	Id           uint      `json:"-" gorm:"primaryKey;not null"`
+	CategoryID   uint      `json:"catrgory_id"`
+	Category     *Category `json:"-"`
+	CategoryName string    `json:"category_name" gorm:"unique;not null" binding:"required,min=3,max=30"`
 }
+type Brand struct {
+	Id        uint   `json:"-" gorm:"primaryKey;not null"`
+	ProductId uint   `json:"product_id"`
+	BrandName string `json:"brand_name" binding:"min=3,max=30"`
+}
+type ProductImage struct {
+	Id            uint    `json:"id" gorm:"primarykey;not null"`
+	ProductId     uint    `json:"product_id" gorm:"not null"`
+	ProductImages string  `json:"product_images"`
+	Product       Product `json:"-"`
+}
+
+// Add On Tables
+/*
+	Brand table
+	Colour Table
+	Size table
+
+	Updation --> Sub Category Table
+*/
