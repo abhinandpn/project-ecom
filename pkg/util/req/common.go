4036dd43a8b60c674e16f113847521edbe8b4c93
abhinandpn <abhinandpn40672gmail.com>
2023-05-14 19:53:36 +0530

    pagenation and block struct added

--- pkg/util/req/common.go
+++ pkg/util/req/common.go
@@ -12,3 +12,16 @@ type Otpverify struct {
 	UserId uint `json:"user_id" binding:"required,numeric"`
 	OTP    uint `json:"otp" binding:"required,min=4,max=6"`
 }
+
+/*
+Page nation for if we have somany user we can show user by group and
+eg: 10 users per page
+each page have diffrents users
+*/
+type PageNation struct {
+	Count      uint `json:"count"`
+	PageNumber uint `json:"page_number"`
+}
+type BlockStruct struct {
+	UserId uint `json:"user_id" binding:"required,numaric"`
+}
