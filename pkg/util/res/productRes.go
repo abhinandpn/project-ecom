49c0a6f16453812488fb8a9193ba9cd7979b0e17
abhinandpn <abhinandpn40672gmail.com>
2023-05-15 21:21:07 +0530

    category and sub categorye

--- pkg/util/res/productRes.go
+++ pkg/util/res/productRes.go
@@ -15,3 +15,16 @@ type ProductResponce struct {
 	CreatedAt     time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
+type CategoryRes struct {
+	Id           uint   `json:"id"`
+	CategoryName string `json:"category_name"`
+}
+type SubCategoryRes struct {
+	Id              uint   `json:"id"`
+	SubcategoryName string `json:"sub_category_name"`
+	CategoryId      uint   `json:"category_id"`
+}
+type BrandRes struct {
+	Id        uint   `json:"id"`
+	BrandName string `json:"brand_name"`
+}
