84789b034bf0eafaa088ebf04d136c4f34643aa5
abhinandpn <abhinandpn40672gmail.com>
2023-05-17 21:53:03 +0530

    user home and user list product

--- pkg/api/routes/user.go
+++ pkg/api/routes/user.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/abhinandpn/project-ecom/pkg/api/handler"
+	"github.com/abhinandpn/project-ecom/pkg/api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,18 +13,26 @@ func UserRoutes(api *gin.RouterGroup, userHandler *handler.UserHandler, productH
 	login := api.Group("/login")
 	{
 		fmt.Println("--------------- route------")
-		login.POST("/", userHandler.UserLogin)
+		login.POST("", userHandler.UserLogin)
 		login.POST("/otp-send", userHandler.UserOtpLogin)
 		login.POST("/otp-verify", userHandler.UserLoginOtpVerify)
 	}
+
 	// Signup
 	signup := api.Group("/signup")
 	{
-		signup.POST("/", userHandler.UserSignUp)
+		signup.POST("", userHandler.UserSignUp)
 	}
-	// Product
-	product := api.Group("/product")
+
+	api.Use(middleware.AuthUser)
 	{
-		product.GET("/all", productHandler.ListProducts) // List all product
+		api.GET("/home", userHandler.UserHome) // User Home
+
+		// Product
+		product := api.Group("/product")
+		{
+			product.GET("/all", productHandler.ListProducts) // List all product
+		}
 	}
+
 }
