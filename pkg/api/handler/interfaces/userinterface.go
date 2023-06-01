e74342c38b0662408858c0a18238a50e454b966b
abhinandpn <abhinandpn40672gmail.com>
2023-06-01 19:59:58 +0530

    updated

--- pkg/api/handler/interfaces/userinterface.go
+++ pkg/api/handler/interfaces/userinterface.go
@@ -5,15 +5,16 @@ import "github.com/gin-gonic/gin"
 type UserHandler interface {
 
 	// user dashbord
-	UserSignUp(ctx *gin.Context)         // signup user
-	UserLogin(ctx *gin.Context)          // login user
-	UserOtpLogin(ctx *gin.Context)       // otp logi for user with number
-	UserLoginOtpVerify(ctx *gin.Context) // otp verify with auth
 	UserHome(ctx *gin.Context)           // user home (verfication)
 	UserInfo(ctx *gin.Context)           // user profile info(login details)
 	UserLogout(ctx *gin.Context)         // user logout
+	UserLogin(ctx *gin.Context)          // login user
+	UserSignUp(ctx *gin.Context)         // signup user
+	UserOtpLogin(ctx *gin.Context)       // otp logi for user with number
+	UserLoginOtpVerify(ctx *gin.Context) // otp verify with auth
 	AddAddress(ctx *gin.Context)         // add address
-	ListAllAddress(ctx *gin.Context)     // list all category - for user
-	UpdateAddress(ctx *gin.Context)      // list all product - for user
+	// address
+	ListAllAddress(ctx *gin.Context) // list all category - for user
+	UpdateAddress(ctx *gin.Context)  // list all product - for user
 	// CreateCart(ctx *gin.Context)
 }
