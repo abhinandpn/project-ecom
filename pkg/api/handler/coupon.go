ff24340eb58016415dcfd898566292c36663244c
abhinandpn <abhinandpn40672gmail.com>
2023-07-30 11:40:57 +0530

    coupon handler

---
+++ pkg/api/handler/coupon.go
@@ -0,0 +1,173 @@
+package handler
+
+import (
+	"net/http"
+
+	handlerInterface "github.com/abhinandpn/project-ecom/pkg/api/handler/interfaces"
+	"github.com/abhinandpn/project-ecom/pkg/helper"
+	service "github.com/abhinandpn/project-ecom/pkg/usecase/interfaces"
+	"github.com/abhinandpn/project-ecom/pkg/util/req"
+	"github.com/abhinandpn/project-ecom/pkg/util/res"
+	"github.com/gin-gonic/gin"
+)
+
+type CouponHandler struct {
+	CouponUseCase service.CouponUseCase
+}
+
+func NewCouponHandler(couponUsecase service.CouponUseCase) handlerInterface.CouponHandler {
+
+	return &CouponHandler{CouponUseCase: couponUsecase}
+}
+
+func (cp *CouponHandler) CrateCoupon(ctx *gin.Context) {
+
+	var body req.CouponReq
+	err := ctx.Bind(&body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "bind faild",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	err = cp.CouponUseCase.AddCoupon(body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cant create coupon",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	respones := res.SuccessResponse(200, "successfully create coupon", body)
+	ctx.JSON(http.StatusOK, respones)
+}
+
+func (cp *CouponHandler) UpdateCoupon(ctx *gin.Context) {
+
+	ParmId := ctx.Param("id")
+	CouponId, err := helper.StringToUInt(ParmId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cant get number",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	var body req.CouponReq
+	err = ctx.Bind(&body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "bind faild",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	coupon, err := cp.CouponUseCase.UpdateCoupon(CouponId, body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "updation failed",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	respones := res.SuccessResponse(200, "successfully create coupon", coupon)
+	ctx.JSON(http.StatusOK, respones)
+}
+
+func (cp *CouponHandler) DeleteCoupon(ctx *gin.Context) {
+
+	ParmId := ctx.Param("id")
+	CouponId, err := helper.StringToUInt(ParmId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cant get number",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	err = cp.CouponUseCase.DeleteCoupon(CouponId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cant get number",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	respones := res.SuccessResponse(200, "successfully delete coupon", CouponId)
+	ctx.JSON(http.StatusOK, respones)
+}
+
+func (cp *CouponHandler) ViewCouponById(ctx *gin.Context) {
+
+	ParmId := ctx.Param("id")
+	CouponId, err := helper.StringToUInt(ParmId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cant get id",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	body, err := cp.CouponUseCase.ViewCouponById(CouponId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cat get coupon",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	respones := res.SuccessResponse(200, "successfully get coupon", body)
+	ctx.JSON(http.StatusOK, respones)
+}
+
+func (cp *CouponHandler) ListCoupon(ctx *gin.Context) {
+
+	body, err := cp.CouponUseCase.ListCoupon()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cat get coupon",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	respones := res.SuccessResponse(200, "successfully get coupon", body)
+	ctx.JSON(http.StatusOK, respones)
+}
+
+func (c *CouponHandler) ViewCouponByCode(ctx *gin.Context) {
+
+	Coupon := ctx.Param("name")
+	body, err := c.CouponUseCase.ViewCouponByCode(Coupon)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, res.Response{
+			StatusCode: 400,
+			Message:    "cat get coupon",
+			Data:       nil,
+			Errors:     err.Error(),
+		})
+		return
+	}
+	respones := res.SuccessResponse(200, "successfully get coupon", body)
+	ctx.JSON(http.StatusOK, respones)
+}
