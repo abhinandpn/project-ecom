03044d9278019eaa33f17964028fc08c33e67f0e
abhinandpn <abhinandpn40672gmail.com>
2023-07-31 10:27:42 +0530

    apply and remove

--- pkg/repository/interface/couponinterface.go
+++ pkg/repository/interface/couponinterface.go
@@ -6,10 +6,15 @@ import (
 )
 
 type Couponrepository interface {
-	AddCoupon(coupon req.CouponReq) error
-	UpdateCoupon(coupon req.CouponReq, Id uint) (domain.Coupon, error)
+	AddCouponWithMoney(coupon req.CouponWithMoney) error
+	UpdateCoupon(coupon req.CouponWithMoney, Id uint) (domain.Coupon, error)
 	DeleteCoupon(couponId uint) error
 	ViewCoupons() ([]domain.Coupon, error)
 	ViewCouponById(couponId uint) (domain.Coupon, error)
-	ViewCouponByCode(code string)(domain.Coupon,error)
+	ViewCouponByCode(code string) (domain.Coupon, error)
+
+	//
+	ApplyCoupon(cid, uid uint) error // cart apply
+	RemoveCoupon(uid uint) error
+	FindCoupon(cid, uid uint) (domain.Coupon, error)
 }
