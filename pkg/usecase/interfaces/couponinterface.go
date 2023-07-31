b1623d88d54933ab2e829d59909bfc1e87b586ae
abhinandpn <abhinandpn40672gmail.com>
2023-07-31 10:28:55 +0530

    coupon apply and remove

--- pkg/usecase/interfaces/couponinterface.go
+++ pkg/usecase/interfaces/couponinterface.go
@@ -6,10 +6,14 @@ import (
 )
 
 type CouponUseCase interface {
-	AddCoupon(coupon req.CouponReq) error
+	AddCoupon(coupon req.CouponWithMoney) error
 	DeleteCoupon(couponId uint) error
 	ViewCouponById(couponId uint) (domain.Coupon, error)
 	ViewCouponByCode(name string) (domain.Coupon, error)
 	ListCoupon() ([]domain.Coupon, error)
-	UpdateCoupon(CouponId uint, coupon req.CouponReq) (domain.Coupon, error)
+	UpdateCoupon(CouponId uint, coupon req.CouponWithMoney) (domain.Coupon, error)
+
+	//
+	ApplyCoupon(code string, uid uint) error
+	RemoveCoupon(code string, uid uint) error
 }
