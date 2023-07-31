4ad9e8ad47e75c4a6687d8dd2522bf89f45dfad5
abhinandpn <abhinandpn40672gmail.com>
2023-07-31 10:29:37 +0530

    coupon apply and remove

--- pkg/api/handler/interfaces/couponinterface.go
+++ pkg/api/handler/interfaces/couponinterface.go
@@ -3,10 +3,14 @@ package interfaces
 import "github.com/gin-gonic/gin"
 
 type CouponHandler interface {
-	CrateCoupon(ctx *gin.Context)
+	CrateCouponWithmoney(ctx *gin.Context)
 	UpdateCoupon(ctx *gin.Context)
 	DeleteCoupon(ctx *gin.Context)
 	ViewCouponById(ctx *gin.Context)
 	ListCoupon(ctx *gin.Context)
 	ViewCouponByCode(ctx *gin.Context)
+
+	//
+	ApplyCoupon(ctx *gin.Context)
+	RemoveCoupon(ctx *gin.Context)
 }
