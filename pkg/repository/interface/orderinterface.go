db1724b46064a485bdf2f19d78587d43cf0c02f5
abhinandpn <abhinandpn40672gmail.com>
2023-08-05 11:58:23 +0530

    order status

--- pkg/repository/interface/orderinterface.go
+++ pkg/repository/interface/orderinterface.go
@@ -2,23 +2,32 @@ package interfaces
 
 import (
 	"github.com/abhinandpn/project-ecom/pkg/domain"
+	"github.com/abhinandpn/project-ecom/pkg/util/res"
 )
 
 type OrderRepository interface {
 	CreateUserOrder(id uint) error
-	FindUserOrderById(uid uint) (domain.UserOrder, error)
-	AddOrderInfo(uid, aid uint, cpid string, price float64, status string) (uint, error)
+	FindUserOrderByUId(uid uint) (domain.UserOrder, error)
+	AddOrderInfo(orderId, AdrsId, CopId uint, price float64, status string, payId uint) (uint, error)
 	FindAllOrderInfoByUid(uid uint) (domain.OrderInfo, error)
 	AddOrderItem(oid, pfid, qty uint) error
-	AddOrderItemCartAll(oid, cid uint) error
 	UpdatePaymentMethod(id, pid uint) error
-
-	// OrderStatus(id, oid uint) (res.OrderStatus, error)
-	// CartOrderAll(cid uint) error
+	CartAllOrder(orderId, OrderinfoId uint, cart []res.CartDisplay) error // updation
+	OrderDetail(uid uint) ([]res.ResOrder, error)
+	ChangeOrderStatus(status string, id uint) error
 
 	/*
 
 		UPDATION NEED ....!
 	*/
 	FindOrderInfoByOrderId(id uint) (domain.OrderInfo, error)
+
+	// Order Status
+	AddOrderStatus(status string) error
+	EditOrderStatus(id uint, status string) (domain.OrderStatus, error)
+	DeleteOrderStatusById(id uint) error
+	GetOrderStatusById(id uint) (domain.OrderStatus, error)
+	GetOrderStatusByStatus(status string) (domain.OrderStatus, error)
+	GetAllOrderStatus() ([]domain.OrderStatus, error)
+	//
 }
