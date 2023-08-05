6c027e86019be74cc033c9c3f1c787e314f33c26
abhinandpn <abhinandpn40672gmail.com>
2023-08-05 11:55:22 +0530

    order status handler

--- pkg/api/handler/interfaces/orderinterface.go
+++ pkg/api/handler/interfaces/orderinterface.go
@@ -7,4 +7,14 @@ type OrderHandler interface {
 	CartAllOrder(ctx *gin.Context)
 	CartOrderStatus(ctx *gin.Context)
 	OrderByproductId(ctx *gin.Context)
+	OrderDetail(ctx *gin.Context)
+
+	// ----------------------
+	CreateOrderStatus(ctx *gin.Context)
+	UpdateOrderStatus(ctx *gin.Context)
+	DeleteOrderStatus(ctx *gin.Context)
+	FindOrderStatusByStatus(ctx *gin.Context)
+	FindOrderStatusById(ctx *gin.Context)
+	GetAllOrderStatus(ctx *gin.Context)
+	// ----------------------
 }
