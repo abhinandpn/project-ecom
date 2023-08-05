03db0b46de9bac8bc744201a4ea07c048e7968db
abhinandpn <abhinandpn40672gmail.com>
2023-08-05 11:58:48 +0530

    order status

--- pkg/usecase/interfaces/orderinterface.go
+++ pkg/usecase/interfaces/orderinterface.go
@@ -1,12 +1,25 @@
 package interfaces
 
-import "github.com/abhinandpn/project-ecom/pkg/util/res"
+import (
+	"github.com/abhinandpn/project-ecom/pkg/domain"
+	"github.com/abhinandpn/project-ecom/pkg/util/res"
+)
 
 type OrderUseCase interface {
 	CreateUserOrder(id uint) error
-	AddOrderInfo(uid, aid uint, cpid string, price float64, status string) (uint, error)
 	AddOrderItems(oid, pfid, qty uint) error
 	OrderByPfId(uid, id uint) error
-	CartOrderAll(uid, pyid uint) error
 	OrderStatus(id, oid uint) (res.OrderStatus, error)
+	CartOrderAll(uid, payid, copId, adrsId uint) error // updation
+	UserOrders(id uint) ([]res.ResOrder, error)
+	ChangeOrderStatus(status string, id uint) error
+
+	// order status
+	CreateOrderStatus(status string) error
+	UpdateOrderStatus(id uint, status string) (domain.OrderStatus, error)
+	DeletOrderStatus(id uint) error
+	FindOrderStatusById(id uint) (domain.OrderStatus, error)
+	FindOrderStatusByStatus(status string) (domain.OrderStatus, error)
+	FindAllOrderStatus() ([]domain.OrderStatus, error)
+	//
 }
