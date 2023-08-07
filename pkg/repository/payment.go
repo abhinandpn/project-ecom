a6c2725f53c20e7b82bc46b0efdb2b64605427ea
abhinandpn <abhinandpn40672gmail.com>
2023-08-07 13:47:44 +0530

    paymnet status update

--- pkg/repository/payment.go
+++ pkg/repository/payment.go
@@ -77,3 +77,68 @@ func (p *PaymentDatabase) GetPaymentMethodById(id uint) (domain.PaymentMethod, e
 	}
 	return body, nil
 }
+
+func (py *PaymentDatabase) AddPaymentStatus(name string) error {
+
+	var body domain.PaymentStatus
+	query := `insert into payment_statuses (payment_status)values ($1);`
+	err := py.DB.Raw(query, name).Scan(&body).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (py *PaymentDatabase) EditPaymentStatus(id uint, name string) (domain.PaymentStatus, error) {
+
+	var body domain.PaymentStatus
+	query := `update payment_statuses set payment_status = $1 where id = $2 ;`
+	err := py.DB.Raw(query, name, id).Scan(&body).Error
+	if err != nil {
+		return body, err
+	}
+	return body, nil
+}
+
+func (py *PaymentDatabase) DeltePaymentStatus(id uint) error {
+
+	query := `delete from payment_statuses where id =$1;`
+	err := py.DB.Exec(query, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (py *PaymentDatabase) GetPaymentStatusByName(name string) (domain.PaymentStatus, error) {
+
+	var body domain.PaymentStatus
+	query := `select * from payment_statuses where payment_status = $1;`
+	err := py.DB.Raw(query, name).Scan(&body).Error
+	if err != nil {
+		return body, err
+	}
+	return body, nil
+}
+
+func (py *PaymentDatabase) GetPaymentStatusById(id uint) (domain.PaymentStatus, error) {
+
+	var body domain.PaymentStatus
+	query := `select * from payment_statuses where id = $1 ;`
+	err := py.DB.Raw(query, id).Scan(&body).Error
+	if err != nil {
+		return body, err
+	}
+	return body, nil
+}
+
+func (py *PaymentDatabase) GetAllPaymentStatus() ([]domain.PaymentStatus, error) {
+
+	var body []domain.PaymentStatus
+	query := `select * from payment_statuses`
+	err := py.DB.Raw(query).Scan(&body).Error
+	if err != nil {
+		return body, err
+	}
+	return body, nil
+}
