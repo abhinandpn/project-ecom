a514a72b601a675344c565aecd974894454b121c
abhinandpn <abhinandpn40672gmail.com>
2023-08-07 13:47:36 +0530

    paymnet status update

--- pkg/repository/interface/paymentinterface.go
+++ pkg/repository/interface/paymentinterface.go
@@ -9,4 +9,13 @@ type PaymentRepository interface {
 	GetPaymentMethodByName(name string) (domain.PaymentMethod, error)
 	DeletePaymentMethod(id uint) error
 	GetPaymentMethodById(id uint) (domain.PaymentMethod, error)
+
+	// -----------------------
+	AddPaymentStatus(name string) error
+	EditPaymentStatus(id uint, name string) (domain.PaymentStatus, error)
+	DeltePaymentStatus(id uint) error
+	GetPaymentStatusByName(name string) (domain.PaymentStatus, error)
+	GetPaymentStatusById(id uint) (domain.PaymentStatus, error)
+	GetAllPaymentStatus() ([]domain.PaymentStatus, error)
+	// -----------------------
 }
