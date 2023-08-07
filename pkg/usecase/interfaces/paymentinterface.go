1446f4494cdfa403462ce81e624669f3347b1a4a
abhinandpn <abhinandpn40672gmail.com>
2023-08-07 13:46:41 +0530

    paymnet status update

--- pkg/usecase/interfaces/paymentinterface.go
+++ pkg/usecase/interfaces/paymentinterface.go
@@ -6,4 +6,13 @@ type PaymentuseCase interface {
 	PaymentMethods() ([]domain.PaymentMethod, error)
 	AddPaymentMethod(name string, status bool) error
 	DeletePaymentMethod(id uint) error
+
+	// ------------------------
+	CreatePaymentStatus(name string) error
+	UpdatePaymentStatus(id uint, name string) (domain.PaymentStatus, error)
+	DeltePaymentStatus(id uint) error
+	FindPaymentStatusById(id uint) (domain.PaymentStatus, error)
+	GetAllPaymentStatus() ([]domain.PaymentStatus, error)
+	// ------------------------
+
 }
