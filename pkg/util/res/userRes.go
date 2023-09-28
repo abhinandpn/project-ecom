25602f0a9d34851618d2cea723e37c2784ac1fec
abhinandpn <abhinandpn40672gmail.com>
2023-09-29 01:57:46 +0530

    updated

--- pkg/util/res/userRes.go
+++ pkg/util/res/userRes.go
@@ -3,10 +3,10 @@ package res
 import "time"
 
 type UserResStruct struct {
-	ID          uint      `json:"id" copier:"must"`
-	FName       string    `json:"first_name" copier:"must"`
-	LName       string    `json:"last_name" copier:"must"`
-	Age         uint      `json:"age" copier:"must"`
+	ID    uint   `json:"id" copier:"must"`
+	FName string `json:"first_name" copier:"must"`
+	LName string `json:"last_name" copier:"must"`
+	// Age         uint      `json:"age" copier:"must"`
 	Email       string    `json:"email" copier:"must"`
 	UserName    string    `json:"user_name" copire:"must"`
 	Number      string    `json:"phone" copier:"must"`
