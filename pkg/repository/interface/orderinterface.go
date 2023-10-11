f58bac8fdbb1f968d7f6f5aab2002c64041b0ed4
abhinandpn <abhinandpn4067@gmail.com>
2023-10-11 10:33:26 +0530

    updsted

--- pkg/repository/interface/orderinterface.go
+++ pkg/repository/interface/orderinterface.go
@@ -8,8 +8,8 @@ import (
 type OrderRepository interface {
 	CreateUserOrder(id uint) error
 	FindUserOrderByUId(uid uint) (domain.UserOrder, error)
-	AddOrderInfo(orderId, AdrsId, CopId uint, price float64, status string, payId uint) (uint, error)
-	FindAllOrderInfoByUid(uid uint) (domain.OrderInfo, error)
+	AddOrderInfo(orderId, AdrsId, CopId uint, price float64, status uint, payid uint) (uint, error)
+	FindOrderInfoByUid(uid uint) (domain.OrderInfo, error)
 	AddOrderItem(oid, pfid, qty uint) error
 	UpdatePaymentMethod(id, pid uint) error
 	CartAllOrder(orderId, OrderinfoId uint, cart []res.CartDisplay) error // updation
@@ -17,7 +17,6 @@ type OrderRepository interface {
 	ChangeOrderStatus(status string, id uint) error
 
 	/*
-
 		UPDATION NEED ....!
 	*/
 	FindOrderInfoByOrderId(id uint) (domain.OrderInfo, error)
@@ -30,4 +29,12 @@ type OrderRepository interface {
 	GetOrderStatusByStatus(status string) (domain.OrderStatus, error)
 	GetAllOrderStatus() ([]domain.OrderStatus, error)
 	//
+	// 01 - 09 - 2023 - Order status updation
+
+	ListOrderDetailByUid(uid uint) ([]res.OrderDetailByUid, error)
+	ListALlOrderByUid(id uint) ([]domain.OrderInfo, error)
+	UpdateOrderStatusToOrdered(id uint) error
+	UpdateOrderStatusToDelivered(id uint) error
+	UpdateOrderStatusToCancelled(id uint) error
+	UpdateOrderStatusToReturned(id uint) error
 }
